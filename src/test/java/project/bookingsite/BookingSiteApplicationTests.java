ae79c755e4c21f77a3ddf589b55ef66c2f301e7b
Mite <mite_mazgaliev@live.com>
2022-02-04 21:28:00 +0100

    Initial commit

---
+++ src/test/java/project/bookingsite/BookingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package project.bookingsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
