aaaa689466e3c19d6cec4090f0171445771e5fcc
Mite <mite_mazgaliev@live.com>
2022-04-21 18:54:54 +0200

    wow

---
+++ src/main/java/com/example/bookingsite/Model/Enum/Role.java
@@ -0,0 +1,7 @@
+package com.example.bookingsite.Model.Enum;
+
+public enum Role {
+    ADMIN,
+    STANDARD,
+    OWNER
+}
---
+++ src/main/java/com/example/bookingsite/Model/Person.java
@@ -0,0 +1,22 @@
+package com.example.bookingsite.Model;
+
+import com.example.bookingsite.Model.Enum.Role;
+
+public class Person {
+
+    Long id;
+
+    String name;
+
+    String username;
+
+    String password;
+
+    String phoneNumber;
+
+    Role userRole;
+
+    //List<Place>
+
+    //List<Reservation>
+}
